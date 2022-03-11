9c686a58a6df94460dad03c9764f4b7c2aac5e93
bardi <bardia.mojra@gmail.com>
2022-03-11 11:42:02 -0600

    Updated with git-express on 2022-03-11.

--- teams_setup.sh
+++ teams_setup.sh
@@ -3,12 +3,11 @@
 sudo apt update
 sudo apt upgrade -y
 
-# install MS teams 
+# install MS teams
 printf '\n\n installing latest ms teams...\n\n'
 curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
 sudo echo "deb [arch=amd64] https://packages.microsoft.com/repos/ms-teams stable main" > /etc/apt/sources.list.d/teams.list
 sudo apt update
-sudo apt install teams
+sudo snap install teams
 
 # EOF
-
