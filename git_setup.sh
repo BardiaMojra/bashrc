2b66772485e307320e44cd0ad64c92d9e545bcc6
bardi <bardia.mojra@gmail.com>
2022-07-12 13:06:56 -0500

    Updated with git-express on 2022-07-12.

--- git_setup.sh
+++ git_setup.sh
@@ -9,37 +9,14 @@ sudo apt clean && sudo apt update # && sudo apt upgrade -y
 # xhost +SI:localuser:root
 
 printf '\n\n\n --->> perform step by step... \n'
+printf '\n\n\n --->> git credentials format: https://user:pass@example.com \n'
 pause
 printf '\n\n\n --->> setting up git config... \n'
 
-git config --global credential.helper 'store --file ~/.my-credentials'
-
-
-
-
-
-
+git config --global credential.helper 'store --file ./../../.git-credentials'
 #git config --global user.email "bardia.mojra@gmail.com"
 #git config --global user.name "bardi"
 #git init #Turn an existing directory into a git repository
-
-sudo apt update
-sudo apt upgrade -y
-
-# printf '\n\n\n --->> installing Google Chrome... \n'
-# wget -O google-chrome.deb 'https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb'
-# sudo dpkg -i google-chrome.deb
-# rm google-chrome.deb
-
-# set MS fonts
-sudo add-apt-repository multiverse
-sudo apt update && sudo apt install ttf-mscorefonts-installer -y
-sudo fc-cache -f -v
-# reinstall if accidentally reject the license agreement
-sudo apt install --reinstall ttf-mscorefonts-installer
-sudo apt-get update --fix-missing
-
-cp ./.bashrc ./../..
-#https://www.displaylink.org/forum/showthread.php?t=67148
+cp ./.git-credentials ./../../.git-credentials
 
 # EOF
