c568c15c26ac0fb97ea3c344a4aba84e5593e0c5
bardia <bardia.mojra@gmail.com>
2022-08-01 14:01:21 -0500

    Updated with git-express on 2022-08-01.

--- gpu_driver_setup.sh
+++ gpu_driver_setup.sh
@@ -3,15 +3,17 @@
 sudo apt update
 sudo apt upgrade -y
 
-printf '\n\n\n --->> setting up latest nvidia driver... \n'
-printf '\n\n\n --->> blacklist XServer nouveau driver... \n'
+printf '--->> setting up latest nvidia driver... '
+printf '--->> blacklist XServer nouveau driver... '
 sudo apt-get purge xserver-xorg-video-intel
 sudo apt-get purge 'nvidia*'
 #sudo bash -c "echo blacklist nouveau > /etc/modprobe.d/blacklist-nvidia-nouveau.conf"
 #sudo bash -c "echo options nouveau modeset=0 >> /etc/modprobe.d/blacklist-nvidia-nouveau.conf"
 
-printf '\n ---->>> Confirm the new modprobe config file...'
+printf '---->>> Confirm the new modprobe config file...'
 #cat /etc/modprobe.d/blacklist-nvidia-nouveau.conf
+printf '---->>> blacklist nouveau'
+printf '---->>> options nouveau modeset=0'
 
 sudo apt autoremove
 
@@ -20,7 +22,7 @@ sudo update-initramfs -u
 sudo ubuntu-drivers devices
 sudo ubuntu-drivers autoinstall
 
-printf '\n\n'
+printf ' '
 printf ' ---->>> now set PRIME mode to performance\n'
 printf '    \--->>> run nvidia-settings \n'
 printf '    \--->>> set PRIME mode to Performance \n'
