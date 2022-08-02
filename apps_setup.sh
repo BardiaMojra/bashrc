1db45c9656cb9044ad8c4d069b1c5a7cb937eb25
bardi <bardia.mojra@gmail.com>
2022-08-01 20:13:42 -0500

    Updated with git-express on 2022-08-01.

--- apps_setup.sh
+++ apps_setup.sh
@@ -13,27 +13,52 @@ fi
 sudo apt clean && sudo apt update && sudo apt upgrade -y
 
 # xhost +SI:localuser:root
-
+echo ' '
+echo ' '
 echo ' --->> install Solaar... '
+echo ' '
+echo ' '
 sudo add-apt-repository ppa:solaar-unifying/stable
 sudo apt update && sudo apt install solaar -y
 
+echo ' '
+echo ' '
 echo ' --->> enable A2DP (High Fidelity) bluetooth audio... '
-sudo apt install pulseaudio pulseaudio-utils pavucontrol pulseaudio-module-bluetooth
+echo ' '
+echo ' '
+sudo apt install pulseaudio pulseaudio-utils pavucontrol pulseaudio-module-bluetooth  -y
 sudo cp ./audio.conf /etc/bluetooth/audio.conf
 sudo service bluetooth restart
 
+echo ' '
+echo ' '
 echo ' --->> upgrade packages... '
+echo ' '
+echo ' '
 sudo apt upgrade -y
 
+echo ' '
+echo ' '
 echo ' --->> install basics... '
+echo ' '
+echo ' '
 sudo apt install wget meld git okular curl tree tmux -y
 sudo apt-get install ubuntu-restricted-extras -y
 
-echo ' --->> install texlive? '
+echo ' '
+echo ' '
+echo ' --->> install texlive '
+echo ' '
+echo ' '
 sudo apt install texlive-full -y
 sudo apt install  gnome-shell-extensions -y
 
+
+echo ' '
+echo ' '
+echo ' --->> install spotify '
+echo ' '
+echo ' '
 curl -sS https://download.spotify.com/debian/pubkey_5E3C45D7B312C643.gpg | sudo apt-key add -
 echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
 sudo apt-get update && sudo apt-get install spotify-client -y
@@ -48,7 +73,6 @@ sudo fc-cache -f -v
 sudo apt install --reinstall ttf-mscorefonts-installer
 sudo apt-get update --fix-missing
 
-cp ./.bashrc ./../..
-#https://www.displaylink.org/forum/showthread.php?t=67148
+cp ./.bashrc ~/.bashrc
 
 # EOF
