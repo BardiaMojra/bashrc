36e35f03d63b92cef12832c7657ac95cf5327aa1
bardi <bardia.mojra@gmail.com>
2022-09-25 21:27:12 -0500

    Updated with git-express on 2022-09-25.

--- conda_setup.sh
+++ conda_setup.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
-# pause function
-function pause(){
+
+function pause(){ # pause function
  read -s -n 1 -p "Press any key to continue..."
  echo ""
 }
+
+if [ "$EUID" -ne 0 ] # check sudo
+  echo ' ' && echo ' '
+  then echo "please run as root..."
+  echo ' ' && echo ' '
+  exit
+fi
+
 sudo apt clean && sudo apt update && sudo apt upgrade -y
-echo ' '
-echo ' '
+
+echo ' ' && echo ' '
 echo ' \---->>> installing latest conda...\n'
-echo ' '
-echo ' '
+echo ' ' && echo ' '
 pause
 #install latest Conda
 wget -O - https://www.anaconda.com/distribution/ 2>/dev/null | sed -ne 's@.*\(https:\/\/repo\.anaconda\.com\/archive\/Anaconda3-.*-Linux-x86_64\.sh\)\">64-Bit (x86) Installer.*@\1@p' | xargs wget
