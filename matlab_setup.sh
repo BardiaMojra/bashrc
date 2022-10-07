4fc11a7209a5e81f4508df52e33dda72520befca
bardi <bardia.mojra@gmail.com>
2022-10-06 21:30:30 -0500

    Updated with git-express on 2022-10-06.

--- matlab_setup.sh
+++ matlab_setup.sh
@@ -4,6 +4,8 @@
 #sudo rm /usr/local/bin/matlab /usr/local/bin/mcc /usr/local/bin/mex /usr/local/bin/mbuild
 #sudo rm -rf /usr/local/MATLAB/R2022b
 
+# in bashrc
+#alias matlab21a="/usr/local/MATLAB/R2021a/bin/matlab . &"
 
 sudo apt clean && sudo apt update && sudo apt upgrade -y
 
@@ -15,23 +17,20 @@ echo '   \---->>> https://www.mathworks.com/licensecenter/licenses/852709/240643
 echo ''
 
 
-sudo mkdir -p /usr/local/MATLAB/R2021a/
+sudo mkdir -p /usr/local/MATLAB/R2022a/
 mkdir ~/Downloads/matlab
 
 sudo unzip ~/Downloads/matlab_*.zip -d ~/Downloads/matlab
 sudo chmod -R 777 ~/Downloads/matlab
 cd ~/Downloads/matlab
 sudo ./install
-sudo chmod a+w -R ~/.matlab/R2021a/
-
-#echo ' \---->>> cloning matlab snippets repo...'
-#cd ~/git && git clone https://github.com/trnkap/matlab-snippets.git matlab-snippets
-#echo ' \---->>> install snippets by using "Install App" inside matlab...'
 
 #sudo chmod a+w -R ~/git/matlab-snippets
 #bash 'matlab-snippets/MATLAB Snippets.mlappinstall'
 
-
+#echo ' \---->>> cloning matlab snippets repo...'
+#cd ~/git && git clone https://github.com/trnkap/matlab-snippets.git matlab-snippets
+#echo ' \---->>> install snippets by using "Install App" inside matlab...'
 
 
 
