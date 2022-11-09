57726e5beb4bd174ef303ea68b2bf7883412bf90
bardi <bardia.mojra@gmail.com>
2022-11-09 10:01:00 -0600

    Updated with git-express on 2022-11-08.

---
+++ gitexpress_allDirs.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+
+### local routines
+function pause(){
+  echo ' ' && echo ' '
+  read -s -n 1 -p "--->> press any key to continue..."
+  echo ' ' && echo ' '
+}
+
+function checkID(){
+  if [ "$EUID" -ne 0 ]
+    echo ' ' && echo ' '
+    then echo "--->> please run as root..."
+    echo ' ' && echo ' '
+    exit
+  fi
+}
+
+
+
+#checkID
+#sudo apt clean && sudo apt update && apt upgrade -y
+
+# xhost +SI:localuser:root
+echo ' ' && echo ' ' && echo '--->> commit all git repos... ' && echo ' '
+pause
+for i in $(ls -d */)
+do
+  echo ${i%%/}
+done
+
+for i in $(ls -d */)
+do
+  echo ${i%%/}
+  cd ${i%%/} && $gitexpress && cd $(pwd)/..
+  echo $(pwd)
+  #echo
+done
+
+# EOF
