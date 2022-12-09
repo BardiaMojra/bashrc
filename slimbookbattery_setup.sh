2a400a70fa3dba456a57a8422616319908e9a2cc
bardi <bardia.mojra@gmail.com>
2022-12-09 13:57:00 -0600

    Updated with git-express on 2022-12-09.

---
+++ slimbookbattery_setup.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+function pause(){
+  echo ' ' && echo ' '
+  read -s -n 1 -p "press any key to continue..."
+  echo ' ' && echo ' '
+}
+
+if [ "$EUID" -ne 0 ]
+  echo ' ' && echo ' '
+  then echo "please run as root..."
+  echo ' ' && echo ' '
+  exit
+fi
+
+sudo apt clean && sudo apt update && apt upgrade -y
+
+# xhost +SI:localuser:root
+echo ' ' && echo ' '
+echo '--->> installing Slimbook Batter applet...'
+echo ' ' && echo ' '
+
+sudo add-apt-repository ppa:slimbook/slimbook
+sudo apt update
+sudo apt install slimbookbattery
+
+echo ' ' && echo ' '
+echo '--->> to remove, run: '
+echo '      sudo apt remove slimbookbattery'
+echo '      sudo add-apt-repository -r ppa:slimbook/slimbook'
+
+
+
+
+# EOF
