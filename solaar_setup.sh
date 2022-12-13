e88da80eb09c126626a06d80761105e5a9c99e1e
bardi <bardia.mojra@gmail.com>
2022-12-13 09:25:51 -0700

    Updated with git-express on 2022-12-13.

--- solaar_setup.sh
+++ solaar_setup.sh
@@ -1,12 +1,34 @@
 #!/bin/bash
 
+function pause(){
+  echo ' ' && echo ' '
+  read -s -n 1 -p "press any key to continue..."
+  echo ' ' && echo ' '
+}
+
+
+sudo apt clean && sudo apt update && apt upgrade -y
+
+# xhost +SI:localuser:rootpause
+echo ' ' && echo ' '
+echo '--->> setting up Solaar... '
+echo ' ' && echo ' '
 
 sudo add-apt-repository ppa:solaar-unifying/stable
 sudo apt-get update
 
-# install solaar 
+# install solaar
 sudo apt install solaar -y
+
+sudo cp 42-logitech-unify-permissions.rules /etc/udev/rules.d
+sudo udevadm control --reload-rules # reload udev rules
+
+echo ' ' && echo ' '
+echo '--->> add solaar to startup applications... '
+echo '--->> Name: Solaar'
+echo '--->> Command: solaar'
+echo ' ' && echo ' '
+
 
 
 # EOF
-
