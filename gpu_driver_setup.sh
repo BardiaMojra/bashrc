13e8df44d23d40e65c2237172fe0505ee587893c
bardi <bardia.mojra@gmail.com>
2022-12-15 20:09:01 -0600

    Updated with git-express on 2022-12-15.

--- gpu_driver_setup.sh
+++ gpu_driver_setup.sh
@@ -16,11 +16,11 @@ echo '--->> setting up latest nvidia driver... '
 echo '--->> blacklist XServer nouveau driver... '
 sudo apt-get purge xserver-xorg-video-intel
 sudo apt-get purge 'nvidia*'
-#sudo bash -c "echo blacklist nouveau > /etc/modprobe.d/blacklist-nvidia-nouveau.conf"
-#sudo bash -c "echo options nouveau modeset=0 >> /etc/modprobe.d/blacklist-nvidia-nouveau.conf"
+sudo bash -c "echo blacklist nouveau > /etc/modprobe.d/blacklist-nvidia-nouveau.conf"
+sudo bash -c "echo options nouveau modeset=0 >> /etc/modprobe.d/blacklist-nvidia-nouveau.conf"
 
 echo '---->>> Confirm the new modprobe config file...'
-#cat /etc/modprobe.d/blacklist-nvidia-nouveau.conf
+cat /etc/modprobe.d/blacklist-nvidia-nouveau.conf
 echo '---->>> blacklist nouveau'
 echo '---->>> options nouveau modeset=0'
 
