4c5866e62551d661c18bce20f3ae0b4cf95261b1
bardi <bardia.mojra@gmail.com>
2023-01-03 14:48:53 -0600

    Updated with git-express on 2023-01-03.

--- env_setup.sh
+++ env_setup.sh
@@ -25,30 +25,39 @@ sudo apt clean && sudo apt update && apt upgrade -y
 
 
 
-sudo -u root -H -s bash refind_spwdetup.sh
-sudo bash basics_setup.sh
-sudo bash git_setup.sh
+sudo -u root -H -s bash refind_setup.sh
+sudo -u root -H -s bash basics_setup.sh
+sudo -u root -H -s bash git_setup.sh
 
 #sudo bash msfonts_setup.sh
 
-sudo bash shell_ext_setup.sh
-#sudo bash vscode_setup.sh
+sudo -u root -H -s bash shell_ext_setup.sh
+#sudo -u root -H -s bash vscode_setup.sh
+<<<<<<< HEAD
 sudo bash gnome_software_setup.sh
 sudo bash teams_setup.sh
 sudo bash bluetooth_drv_setup.sh
 sudo bash slimbookbattery_setup.sh
 sudo bash solaar_setup.sh
 sudo bash spotify_setup.sh
+=======
+sudo -u root -H -s bash gnome_software_setup.sh
+sudo -u root -H -s bash teams_setup.sh
+sudo -u root -H -s bash bluetooth_drv_setup.sh
+sudo -u root -H -s bash slimbookbattery_setup.sh
+sudo -u root -H -s bash solaar_setup.sh
+sudo -u root -H -s bash spotify_setup.sh
+>>>>>>> 7ce2a36f902a962dc48e47825c32fa257c740359
 
 
-cp ./.bashrc ~/.bashrc
-source ~/.bashrc
+sudo -u root -H -s cp ./.bashrc ~/.bashrc
+sudo -u root -H -s source ~/.bashrc
 
 
 echo ' ' && echo ' '
 echo '--->> install shell extensions...'
 echo ' ' && echo ' '
-okular shell_ext.pdf
+okular shell_ext.pdf &
 pause
 
 echo ' ' && echo ' '
