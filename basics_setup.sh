9769d657da7c216b0652ccfa89d62c93d52fc21c
bardi <bardia.mojra@gmail.com>
2023-01-22 21:14:52 -0600

    Updated with git-express on 2023-01-22.

--- basics_setup.sh
+++ basics_setup.sh
@@ -13,6 +13,8 @@ echo ' ' && echo ' '
 sudo apt install wget meld git okular curl tree tmux pandoc -y
 sudo apt-get install ubuntu-restricted-extras -y
 sudo apt install gparted -y
+sudo apt install texlive-latex-extra -y
+
 #sudo snap install vlc -y
 #sudo apt install wine winetricks -y
 #sudo apt install steam
