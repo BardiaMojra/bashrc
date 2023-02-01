5a76374a1534c336101d5f2f7fd82b6f791471c6
bardi <bardia.mojra@gmail.com>
2023-01-31 23:37:37 -0600

    Updated with gitexpress on 2023-01-31.

--- solaar_setup.sh
+++ solaar_setup.sh
@@ -12,6 +12,23 @@ if [ "$EUID" -ne 0 ]
 fi
 
 
+
+#todo ----------------------------------------------------------------->> installing solaar
+#todo install script (perhaps full) from mybash
+# sudo apt install udev python3-pyudev python3-psutil python3-xlib python3-yaml \
+# python3-gi gir1.2-gtk-3.0 python3-dev gir1.2-notify-0.7 \
+# gir1.2-ayatanaappindicator3-0.1 gir1.2-ayatanaappindicator3-0.1 \
+# gir1.2-appindicator3-0.1
+# then do:
+# You can install this rule by copying, as root, rules.d/42-logitech-unify-permissions.rules from Solaar to /etc/udev/rules.d. You will probably also have to tell udev to reload its rule via sudo udevadm control --reload-rules.
+echo ' ' && echo ' '
+echo '--->> setting up Solaar... '
+echo ' ' && echo ' '
+echo ' ' && echo ' '
+echo '--->> read todo note at ./solaar_setup.sh.'
+echo ' ' && echo ' '
+pause
+
 sudo apt clean && sudo apt update && apt upgrade -y
 
 # xhost +SI:localuser:rootpause
