a634d6b43da3d65d70b044e340110e0556518d8e
bardi <bardia.mojra@gmail.com>
2023-03-15 10:35:08 -0600

    Updated with gitexpress on 2023-03-15.

--- docker_postinstall_setup.sh
+++ docker_postinstall_setup.sh
@@ -7,14 +7,10 @@ function pause(){
 
 # sudo apt clean && sudo apt update && apt upgrade -y
 
-
-
 echo ' ' && echo ' '
 echo ' \---->>> installing docker post-install (group permissions)...'
 echo 'for more details see: https://docs.docker.com/engine/install/linux-postinstall/'
 echo ' ' && echo ' '
-
-
 
 # Create the docker group.
 sudo -u root -H -s groupadd docker
@@ -46,7 +42,6 @@ echo " This error indicates that the permission settings for the ~/.docker/ dire
 echo " To fix this problem, either remove the ~/.docker/ directory (it's recreated automatically, but any custom settings are lost), or change its ownership and permissions using the following commands:"
 echo '    sudo chown "$USER":"$USER" /home/"$USER"/docker -R'
 echo '    sudo chmod g+rwx "$HOME/docker" -R'
-
 
 
 echo ' ' && echo ' '
