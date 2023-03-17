74248442d5d7327671cfef6899e8d939368cc494
bardi <bardia.mojra@gmail.com>
2023-03-17 16:12:03 -0500

    Updated with gitexpress on 2023-03-17.

--- conda_setup.sh
+++ conda_setup.sh
@@ -14,18 +14,20 @@ function pause(){ # pause function
 
 sudo apt clean && sudo apt update && sudo apt upgrade -y
 
-echo ' ' && echo ' '
-echo ' \---->>> installing latest conda...'
-echo ' ' && echo ' '
+echo ' ' && echo ' ' && echo ' \---->>> installing latest conda...' && echo ' ' && echo ' '
 pause
 #install latest Conda
 wget -O - https://www.anaconda.com/distribution/ 2>/dev/null | sed -ne 's@.*\(https:\/\/repo\.anaconda\.com\/archive\/Anaconda3-.*-Linux-x86_64\.sh\)\">64-Bit (x86) Installer.*@\1@p' | xargs wget
 
 mv Anaconda3*.sh conda3.sh
 sudo chmod +x conda3.sh
-bash conda3.sh
-
-rm conda3.sh
+echo ' ' && echo ' '
+echo ' \---->>> run:'
+echo ' bash conda3.sh && rm conda3.sh'
+echo ' ' && echo ' '
+pause
+# bash conda3.sh
+# rm conda3.sh
 
 
 
